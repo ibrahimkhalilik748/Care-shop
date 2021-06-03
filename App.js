7d290a3bbe9d75aefdfefa8437e8ba53c753cff9
Ibrahim Khalil <ibrahimkhalilik748@gmail.com>
2021-06-03 22:52:01 +0600

    b

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, ImageBackground, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      
+    </View >
   );
 }
 
@@ -18,4 +17,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  body: {
+    marginTop: 100,
+  }
 });
